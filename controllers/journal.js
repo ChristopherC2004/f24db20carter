ff9c66c2fe2f30072b1340126f614bb0aeb38cff
Christopher Carter <S551122@nwmissouri.edu>
2024-11-19 19:51:16 -0600

    Think I got update set up.

--- controllers/journal.js
+++ controllers/journal.js
@@ -123,3 +123,17 @@ exports.journal_create_Page = function(req, res) {
     }
 };
     
+// Handle building the view for updating a journal.
+// query provides the id
+exports.journal_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Journal.findById(req.query.id)
+    res.render('journalupdate', { title: 'Journal Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
--- routes/journal.js
+++ routes/journal.js
@@ -11,5 +11,8 @@ router.get('/detail', journal_controller.journal_view_one_Page);
 /* GET create journal page */
 router.get('/create', journal_controller.journal_create_Page);
 
+/* GET create update page */
+router.get('/update', journal_controller.journal_update_Page);
+
 
 module.exports = router;
