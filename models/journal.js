6a61b272a20eb9afccc2123c87b2f965438c6614
Christopher Carter <S551122@nwmissouri.edu>
2024-12-03 21:50:00 -0600

    did the two bonus features

--- models/journal.js
+++ models/journal.js
@@ -1,13 +1,22 @@
 const mongoose = require("mongoose");
 const journalSchema = mongoose.Schema({
-coverMaterial: String,
+coverMaterial: {
+    type: String,
+    enum: {
+        values: ['Leather', 'Hardcover', 'Softcover'], // Allowed values
+      },
+    required: true,
+},
 pages: {
     type: Number,
-    required: true, // Ensure the field is mandatory
-    min: [30, "'pages' must be at least 30"], // Minimum value
-    max: [1000, "'pages' cannot exceed 1000"], // Maximum value
-    },
-cost: Number 
+    required: true, 
+    min: [30], // Minimum value
+    max: [1000], // Maximum value
+},
+cost:{ 
+    type: Number,
+    required: true,
+} 
 });
 module.exports = mongoose.model("Journal",
 journalSchema);
--- routes/journal.js
+++ routes/journal.js
@@ -17,12 +17,12 @@ router.get('/', journal_controller.journal_view_all_Page);
 router.get('/detail', journal_controller.journal_view_one_Page);
 
 /* GET create journal page */
-router.get('/create', journal_controller.journal_create_Page);
+router.get('/create',secured, journal_controller.journal_create_Page);
 
 /* GET create update page */
 router.get('/update',secured, journal_controller.journal_update_Page);
 
 /* GET delete journal page */
-router.get('/delete', journal_controller.journal_delete_Page);
+router.get('/delete',secured, journal_controller.journal_delete_Page);
 
 module.exports = router;
