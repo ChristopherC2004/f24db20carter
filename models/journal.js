26fc511fcc28bedca057479fc6be9dca7f61fe50
Christopher Carter <S551122@nwmissouri.edu>
2024-12-03 21:20:16 -0600

    added new buttons to journal.pug, fixed some messed up links, added validation for page numbers

--- models/journal.js
+++ models/journal.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 const journalSchema = mongoose.Schema({
 coverMaterial: String,
-pages: Number,
-cost: Number
+pages: {
+    type: Number,
+    required: true, // Ensure the field is mandatory
+    min: [30, "'pages' must be at least 30"], // Minimum value
+    max: [1000, "'pages' cannot exceed 1000"], // Maximum value
+    },
+cost: Number 
 });
 module.exports = mongoose.model("Journal",
 journalSchema);
